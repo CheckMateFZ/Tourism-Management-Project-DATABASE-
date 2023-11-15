7aa420b154e32468712957bf962ed2f6b0327126
seher sidd <sehersidd87@gmail.com>
2023-11-15 13:07:17 +0500

    jj
    hhh

---
+++ pages/sign-up-login-form/signup.js
@@ -0,0 +1,50 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const signUpForm = document.querySelector(".signup form");
+  
+    signUpForm.addEventListener("submit", async function (event) {
+      event.preventDefault();
+  
+      const usernameInput = signUpForm.querySelector('input[name="username"]');
+      const passwordInput = signUpForm.querySelector('input[name="password"]');
+      const emailInput = signUpForm.querySelector('input[name="email"]');
+  
+      // Check if the inputs are not null before accessing their values
+      if (usernameInput && passwordInput && emailInput) {
+        const username = usernameInput.value;
+        const password = passwordInput.value;
+        const email = emailInput.value;
+  
+        try {
+          const response = await fetch("http://localhost:3000/createUser", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              username: username,
+              password: password,
+              email: email,
+            }),
+          });
+  
+          if (response.status === 201) {
+            console.log("User created successfully");
+            // You can add code here to handle the case of successful user creation, like showing a success message or redirecting the user to the login page.
+          } else if (response.status === 409) {
+            console.log("User already exists");
+            // You can add code here to handle the case of a user already existing with the provided username.
+          } else {
+            console.error("Failed to create user");
+            // You can add code here to handle other error cases.
+          }
+        } catch (error) {
+          console.error("Error:", error);
+        }
+      } else {
+        console.error("One or more form inputs are null");
+      }
+    });
+  
+    // ... (other existing code)
+  });
+  
