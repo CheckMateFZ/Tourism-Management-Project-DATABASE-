0d0ac83ba5c5ad23a43db29e0f786e4a627d6487
seher sidd <sehersidd87@gmail.com>
2023-11-15 12:48:01 +0500

    Update script.js

--- pages/sign-up-login-form/script.js
+++ pages/sign-up-login-form/script.js
@@ -7,57 +7,36 @@ document.addEventListener("DOMContentLoaded", function () {
   signUpForm.addEventListener("submit", async function (event) {
     event.preventDefault();
 
-    // ... (your existing signup code)
-  });
-
-  loginForm.addEventListener("submit", async function (event) {
-    event.preventDefault();
-
-    const email = loginForm.querySelector('input[name="email"]').value;
-    const password = loginForm.querySelector('input[name="pswd"]').value;
+    const username = signUpForm.querySelector('input[name="username"]').value;
+    const password = signUpForm.querySelector('input[name="password"]').value;
+    const email = signUpForm.querySelector('input[name="email"]').value;
 
     try {
-      const response = await fetch("http://localhost:3000/login", {
+      const response = await fetch("http://localhost:3000/createUser", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          email: email,
+          username: username,
           password: password,
+          email: email,
         }),
       });
 
-      if (response.status === 200) {
-        console.log("User logged in successfully");
-        
-        // Redirect the user to the previous page
-        const previousPage = sessionStorage.getItem("previousPage");
-        if (previousPage) {
-          window.location.href = previousPage;
-        } else {
-          // If there's no previous page, you can redirect to a default page
-          window.location.href = "/";
-        }
-
-        // Clear the previous page from sessionStorage
-        sessionStorage.removeItem("previousPage");
-      } else if (response.status === 401) {
-        console.log("Invalid credentials");
-        // You can add code here to handle the case of invalid credentials
+      if (response.status === 201) {
+        console.log("User created successfully");
+        // You can add code here to handle the case of successful user creation, like showing a success message or redirecting the user to the login page.
+      } else if (response.status === 409) {
+        console.log("User already exists");
+        // You can add code here to handle the case of a user already existing with the provided username.
       } else {
-        console.error("Failed to log in");
-        // You can add code here to handle other error cases
+        console.error("Failed to create user");
+        // You can add code here to handle other error cases.
       }
     } catch (error) {
       console.error("Error:", error);
     }
-  });
-
-  // Store the current page in sessionStorage when navigating to the login page
-  const loginLink = document.querySelector(".login label[for='chk']");
-  loginLink.addEventListener("click", function () {
-    sessionStorage.setItem("previousPage", window.location.href);
   });
 
   // ... (other existing code)
